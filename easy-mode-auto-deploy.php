f901ca4bc80a3668f3d38ba87da5069412e8c367
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-02 14:30:30 -0800

    Initial commit

---
+++ easy-mode-auto-deploy.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Easy Mode Auto Deploy
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: easy-mode-auto-deploy
+Domain Path: /languages
+*/
+
